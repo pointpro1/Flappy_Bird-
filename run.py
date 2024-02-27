6601fa9637528ecb3c78771ada9f40342aae12a2
User <pitpet123@gmail.com>
2024-02-27 20:49:20 +0300

    добавили спрайты, анимацию прыжка, обработку анимации и гравитацию игры

--- run.py
+++ run.py
@@ -34,6 +34,81 @@ bg = pygame.image.load('img/fon1.png')
 ground_img = pygame.image.load('img/ground.png')
 #!!!! button_img = pygame.image.load('img/restart.png')
 
+# Создание спрайтов
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'img/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.vel = 0
+        self.clicked = False
+
+    def update(self):
+
+        # Гравитация игры
+
+        if flying is True:
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
+
+        if game_over is False:
+            # Анимация прыжка
+
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
+            # Обработка анимации
+            self.counter += 1
+            flap_cooldown = 5
+
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+            self.image = self.images[self.index]
+
+            self.image = pygame.transform.rotate(self.images[self.index], int(self.vel * -2))
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index], -90)
+
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/pipe.png')
+        self.rect = self.image.get_rect()
+        # позиция 1 - сверху, -1 - снизу
+        if position == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
+        if position == -1:
+            self.rect.topleft = [x, y + int(pipe_gap / 2)]
+
+    def update(self):
+        self.rect.x -= scroll_speed
+        if self.rect.right < 0:
+            self.kill()
+
+bird_group = pygame.sprite.Group()
+pipe_group = pygame.sprite.Group()
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
 
 run = True
 while run:
