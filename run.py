19450b78b6f7f756642476b2e3b468b724b8f333
User <pitpet123@gmail.com>
2024-02-27 21:11:57 +0300

    добавили проверку результатов и рестарт игры

--- run.py
+++ run.py
@@ -33,7 +33,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Flappy Bird')
 bg = pygame.image.load('img/fon1.png')
 ground_img = pygame.image.load('img/ground.png')
-#!!!! button_img = pygame.image.load('img/restart.png')
+button_img = pygame.image.load('img/restart.png')
 
 def draw_text(text, font, text_color, x, y):
     img = font.render(text, True, text_color)
@@ -125,29 +125,71 @@ flappy = Bird(100, int(screen_height / 2))
 bird_group.add(flappy)
 
 
-# Основной игровой цикл
+# - Основной игровой цикл -
+
+# Запуск игры
 run = True
 while run:
+
     clock.tick(fps)
 
     screen.blit(bg, (0, 0))
+
     bird_group.draw(screen)
     bird_group.update()
+
     pipe_group.draw(screen)
+
+    # ground_img
     screen.blit(ground_img, (ground_scroll, 768))
 
-    # Проверка коллизий
+    # Проверка результата
+    if len(pipe_group) > 0:
+        if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left \
+                and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right \
+                and pass_pipe == False:
+            pass_pipe = True
+        if pass_pipe == True:
+            if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
+                score += 1
+                pass_pipe = False
+
+    draw_text(str(score), font, white, int(screen_width / 2), 20)
+
+    # проверка колиции(столкновения)
     if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or flappy.rect.top < 0:
         game_over = True
+
+    # проверка, что птица не ударилась о землю
     if flappy.rect.bottom >= 768:
         game_over = True
         flying = False
+
+    if game_over == False and flying == True:
+        # Генерирация труб(стволов)
+        time_now = pygame.time.get_ticks()
+        if time_now - last_pipe > pipe_frequency:
+            pipe_height = random.randint(-100, 100)
+            btm_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, -1)
+            top_pipe = Pipe(screen_width, int(screen_height / 2) + pipe_height, 1)
+            pipe_group.add(btm_pipe, top_pipe)
+            last_pipe = time_now
+
+        ground_scroll -= scroll_speed
+        if abs(ground_scroll) > 35:
+            ground_scroll = 0
+        pipe_group.update()
+
+    # Проверка результата и рестрат игры
+   # !!!
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
             flying = True
+
+    print(score)
 
     pygame.display.update()
 
